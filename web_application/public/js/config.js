7d8d3d2b0be6449fa038ffab6f7e08f23fd2547e
evercx <306534137@qq.com>
2017-04-27 19:08:31 +0800

    添加图表统计

--- web_application/public/js/config.js
+++ web_application/public/js/config.js
@@ -14,28 +14,107 @@
 // 	}
 // });
 
-var school_list={
-	'smu':'上海海事大学',
-	'sjtu':'上海交通大学',
-	'tongji':'同济大学',
-	'fudan':'复旦大学',
-	'ecnu':'华东师范大学',
-	'shu':'上海大学',
-	'ecust':'华东理工大学',
-	'dhu':'东华大学',
-	'shufe':'上海财经大学',
-	'shisu':'上海外国语大学',
-	'ecupl':'华东政法大学',
-	'shnu':'上海师范大学',
-	'usst':'上海理工大学',
-	'shou':'上海海洋大学',
-	'shutcm':'上海中医药大学',
-	'shcmusic':'上海音乐学院',
-	'sta':'上海戏剧学院',
-	'shift':'上海对外经贸大学',
-	'sdju':'上海电机学院',
-	'sues':'上海工程技术大学'
-};
+var school_list = [
+	{
+		"zh_name": "上海海事大学",
+		"en_name": "smu"
+	},
+
+	{
+		"zh_name": "上海交通大学",
+		"en_name": "sjtu"
+	},
+
+	{
+		"zh_name": "同济大学",
+		"en_name": "tongji"
+	},
+
+	{
+		"zh_name": "复旦大学",
+		"en_name": "fudan"
+	},
+
+	{
+		"zh_name": "华东师范大学",
+		"en_name": "ecnu"
+	},
+
+	{
+		"zh_name": "上海大学",
+		"en_name": "shu"
+	},
+
+	{
+		"zh_name": "华东理工大学",
+		"en_name": "ecust"
+	},
+
+	{
+		"zh_name": "东华大学",
+		"en_name": "dhu"
+	},
+
+	{
+		"zh_name": "上海财经大学",
+		"en_name": "shufe"
+	},
+
+	{
+		"zh_name": "上海外国语大学",
+		"en_name": "shisu"
+	},
+
+	{
+		"zh_name": "华东政法大学",
+		"en_name": "ecupl"
+	},
+
+	{
+		"zh_name": "上海师范大学",
+		"en_name": "shnu"
+	},
+
+	{
+		"zh_name": "上海理工大学",
+		"en_name": "usst"
+	},
+
+	{
+		"zh_name": "上海海洋大学",
+		"en_name": "shou"
+	},
+
+	{
+		"zh_name": "上海中医药大学",
+		"en_name": "shutcm"
+	},
+
+	{
+		"zh_name": "上海音乐学院",
+		"en_name": "sus"
+	},
+
+	{
+		"zh_name": "上海戏剧学院",
+		"en_name": "sta"
+	},
+
+	{
+		"zh_name": "上海对外经贸大学",
+		"en_name": "shift"
+	},
+
+	{
+		"zh_name": "上海电机学院",
+		"en_name": "sdju"
+	},
+
+	{
+		"zh_name": "上海工程技术大学",
+		"en_name": "sues"
+	}
+]
 
 var classification = {
 	study:"学习学术",
---
+++ web_application/public/js/statistics_page.js
@@ -0,0 +1,131 @@
+
+
+
+var tableListApp = new Vue({
+	el:"#tableList",
+	data:{
+		dataList:[]
+	},
+	created:function() {
+		var _self = this;
+		axios.get('/api/v1/newsnumber').then(function (res) {
+			_self.dataList = res.data;
+			console.log(this);
+			console.log(_self);
+		})
+	}
+})
+
+var newsProportionChart = echarts.init(document.getElementById('newsProportion'),'shine');
+
+var option = {
+
+	title : {
+		text: '各分类下新闻所占比',
+		x:'center',
+		textStyle: {
+			color: '#ff6600'
+		}
+	},
+	tooltip : {
+		trigger: 'item',
+		formatter: "{b} : {c} ({d}%)"
+	},
+	legend: {
+		orient: 'vertical',
+		left: 'left',
+		data: ['社会新闻','招生考试','社会活动','学习学术']
+	},
+	series : [
+		{
+			//name: '访问来源',
+			type: 'pie',
+			radius : '55%',
+			center: ['50%', '60%'],
+			data:[
+				{value:335, name:'社会新闻'},
+				{value:310, name:'招生考试'},
+				{value:234, name:'社会活动'},
+				{value:135, name:'学习学术'},
+			],
+			itemStyle: {
+				emphasis: {
+					shadowBlur: 10,
+					shadowOffsetX: 0,
+					shadowColor: 'rgba(0, 0, 0, 0.5)'
+				}
+			}
+		}
+	]
+}
+
+function getSocialCount(){
+	return axios({
+		method: 'get',
+		url: '/api/v1/news/count',
+		params:{
+			query:{
+				"classification":"social"
+			}
+		}
+	})
+}
+function getActivityCount(){
+	return axios({
+		method: 'get',
+		url: '/api/v1/news/count',
+		params:{
+			query:{
+				"classification":"activity"
+			}
+		}
+	})
+}
+function getEntranceCount(){
+	return axios({
+		method: 'get',
+		url: '/api/v1/news/count',
+		params:{
+			"query":{
+				"classification":"entrance"
+			}
+		}
+	})
+}
+function getStudyCount(){
+	return axios({
+		method: 'get',
+		url: '/api/v1/news/count',
+		params:{
+			query:{
+				"classification":"study"
+			}
+		}
+	})
+}
+
+axios.all([getSocialCount(),getActivityCount(),getEntranceCount(),getStudyCount()])
+	.then(axios.spread(function(socialRes,activityRes,entranceRes,studyRes){
+
+		newsProportionChart.setOption({
+			series : [
+				{
+					type: 'pie',
+					radius : '55%',
+					center: ['50%', '60%'],
+					data:[
+						{value:socialRes.data.count, name:'社会新闻'},
+						{value:entranceRes.data.count, name:'招生考试'},
+						{value:activityRes.data.count, name:'社会活动'},
+						{value:studyRes.data.count, name:'学习学术'},
+					]
+				}
+			]
+		})
+	}))
+
+newsProportionChart.setOption(option);
+
+
+
+
