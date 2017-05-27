68dbc7d237dfb0b2cf311d9e5c93d59c39651201
evercx <306534137@qq.com>
2017-05-27 12:23:28 +0800

    修改数据库地址

--- web_application/ipConfig.js
+++ web_application/ipConfig.js
@@ -5,7 +5,7 @@
 var IPAddress = {
     //mongodb:'mongodb://localhost:27017/poa/'
     //mongodb:'mongodb://121.42.236.250:27034/POA/'
-    mongodb:'mongodb://121.42.236.250:27034/ResultPOA/'
+    mongodb:'mongodb://127.0.0.1:27034/ResultPOA/'
     
 };
 
